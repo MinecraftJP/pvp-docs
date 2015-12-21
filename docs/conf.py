e626a433ea6ff1cb46d60013d199ba7c9110a52b
ayunyan <ayunyan@gmail.com>
2015-12-21 13:27:44 +0900

    Move apiexplorer dir

--- docs/conf.py
+++ docs/conf.py
@@ -137,12 +137,12 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'apiexplorer']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ['../apiexplorer']
+html_extra_path = ['apiexplorer']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
